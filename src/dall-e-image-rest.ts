4dd0cbfc1023c840a776e54be8f65078c4c5e135
Michael Sprague <mikesprague@gmail.com>
2023-05-02 11:26:33 -0400

    refactor: remove self calling function wrapper

--- src/dall-e-image-rest.ts
+++ src/dall-e-image-rest.ts
@@ -7,8 +7,6 @@ import got from 'got';
 import { wait } from './lib/helpers.js';
 
 // define typescript resources
-declare let imageOperationResponseBody: ImageOperationResponse;
-
 interface ImageOperationResponseResult {
   caption: string;
   contentUrl?: string;
@@ -36,99 +34,96 @@ if (!OPENAI_BASE_PATH || !OPENAI_API_KEY || !OPENAI_AZURE_DALLE_API_VERSION) {
   process.exit(1);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-(async () => {
-  const { hrtime } = process;
-  const debugStartTime = hrtime();
+const { hrtime } = process;
+const debugStartTime = hrtime();
 
-  // set common request headers used in multiple requests
-  const requestHeaders = {
-    'api-key': OPENAI_API_KEY,
-    'User-Agent': 'cloud-team-automation',
-  };
+// set common request headers used in multiple requests
+const requestHeaders = {
+  'api-key': OPENAI_API_KEY,
+  'User-Agent': 'cloud-team-automation',
+};
 
-  // build the endpoint URL
-  const apiUrl = `${OPENAI_BASE_PATH}dalle/text-to-image?api-version=${OPENAI_AZURE_DALLE_API_VERSION}`;
+// build the endpoint URL
+const apiUrl = `${OPENAI_BASE_PATH}dalle/text-to-image?api-version=${OPENAI_AZURE_DALLE_API_VERSION}`;
 
-  // set the prompt for the DALL-E image generation
-  const imagePrompt = oneLineTrim`
+// set the prompt for the DALL-E image generation
+const imagePrompt = oneLineTrim`
     Detailed image of a clocktower with a pumpkin on the very top of it's spire
   `;
 
-  // make request to generate the image
-  const createImageResponse = await got({
-    method: 'POST',
-    url: apiUrl,
+// make request to generate the image
+const createImageResponse = await got({
+  method: 'POST',
+  url: apiUrl,
+  headers: {
+    ...requestHeaders,
+    'Content-Type': 'application/json',
+  },
+  json: {
+    caption: imagePrompt,
+    resolution: '1024x1024',
+    format: 'b64_json',
+  },
+});
+
+// extract operation url from the response headers
+const imageOperationUrl = createImageResponse.headers[
+  'operation-location'
+] as string;
+// console.log(imageOperationUrl);
+
+// set a couple variables used in the loop
+let retryNumber = 0;
+let imageUrl = undefined;
+
+// make requests to the operation url until the image url is returned
+while (imageUrl === undefined) {
+  const imageOperationResponse = await got({
+    url: imageOperationUrl,
+    method: 'GET',
     headers: {
       ...requestHeaders,
       'Content-Type': 'application/json',
     },
-    json: {
-      caption: imagePrompt,
-      resolution: '1024x1024',
-      format: 'b64_json',
-    },
   });
 
-  // extract operation url from the response headers
-  const imageOperationUrl = createImageResponse.headers[
-    'operation-location'
-  ] as string;
-  // console.log(imageOperationUrl);
+  const imageOperationResponseBody = JSON.parse(
+    imageOperationResponse.body,
+  ) as ImageOperationResponse;
 
-  // set a couple variables used in the loop
-  let retryNumber = 0;
-  let imageUrl = undefined;
+  const imageResult: ImageOperationResponseResult =
+    imageOperationResponseBody.result;
+  imageUrl = imageResult.contentUrl;
+  // exponentially back-off based on retry number
+  await wait(2 ** retryNumber * 10);
+  retryNumber += 1;
+}
+// console.log(imageUrl);
 
-  // make requests to the operation url until the image url is returned
-  while (imageUrl === undefined) {
-    const imageOperationResponse = await got({
-      url: imageOperationUrl,
-      method: 'GET',
-      headers: {
-        ...requestHeaders,
-        'Content-Type': 'application/json',
-      },
-    });
-
-    const imageOperationResponseBody = JSON.parse(
-      imageOperationResponse.body,
-    ) as ImageOperationResponse;
-
-    const imageResult: ImageOperationResponseResult =
-      imageOperationResponseBody.result;
-    imageUrl = imageResult.contentUrl;
-    // exponentially back-off based on retry number
-    await wait(2 ** retryNumber * 10);
-    retryNumber += 1;
-  }
-  // console.log(imageUrl);
-
-  // create unique id to use for image name
-  const imageId = uuid();
-  // request completed image
-  await got({
-    url: imageUrl,
-    method: 'GET',
-    responseType: 'text',
+// create unique id to use for image name
+const imageId = uuid();
+// request completed image
+await got({
+  url: imageUrl,
+  method: 'GET',
+  responseType: 'text',
+  encoding: 'base64',
+  headers: {
+    ...requestHeaders,
+  },
+}).then((response) => {
+  // write image to local file
+  fs.writeFileSync(`./generated-images/${imageId}.png`, response.body, {
     encoding: 'base64',
-    headers: {
-      ...requestHeaders,
-    },
-  }).then((response) => {
-    // write image to local file
-    fs.writeFileSync(`./generated-images/${imageId}.png`, response.body, {
-      encoding: 'base64',
-    });
-    // output original prompt and image location
-    console.log(imagePrompt);
-    console.log(`Image saved to: '../generated-images/${imageId}.png'`);
   });
+  // output original prompt and image location
+  console.log(imagePrompt);
+  console.log(`Image saved to: '../generated-images/${imageId}.png'`);
+});
 
-  // set and output debug/timing info
-  const debugEndTime = hrtime(debugStartTime);
-  const debugOutput = `\nExecution time: ${
-    debugEndTime[0] * 1000 + debugEndTime[1] / 1000000
-  }ms`;
-  console.log(debugOutput);
-})();
+// set and output debug/timing info
+const debugEndTime = hrtime(debugStartTime);
+const debugOutput = `\nExecution time: ${
+  debugEndTime[0] * 1000 + debugEndTime[1] / 1000000
+}ms`;
+console.log(debugOutput);
