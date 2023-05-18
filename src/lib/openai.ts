fc4083162d8b98c438dd6f5862615e315444cc5c
Michael Sprague <mikesprague@gmail.com>
2023-05-18 15:34:10 -0400

    docs(openai): add jsdoc comments

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -77,6 +77,14 @@ export interface SaveImageGenerationResultParams {
 }
 
 // helper methods
+
+/**
+ * defaultHeaders
+ * @description returns default headers for reuse in Azure OpenAI API requests
+ * @example const headers = defaultHeaders();
+ * @param {string} [userAgent='cloud-team-automation']  The user agent to use in the headers.
+ * @returns {Headers} An object containing the headers
+ */
 export const defaultHeaders = (
   userAgent = 'cloud-team-automation',
 ): Headers => {
@@ -96,6 +104,12 @@ export const defaultHeaders = (
   };
 };
 
+/**
+ * imageGenerationUrl
+ * @description This function returns the URL for generating images using Azure OpenAI's DALL-E API.
+ * @example const url = imageGenerationUrl();
+ * @returns {string} The URL for generating images.
+ */
 export const imageGenerationUrl = (): string => {
   if (!OPENAI_BASE_PATH || !OPENAI_AZURE_DALLE_API_VERSION) {
     throw new Error(
@@ -111,6 +125,12 @@ export const imageGenerationUrl = (): string => {
   return `${OPENAI_BASE_PATH}dalle/text-to-image?api-version=${OPENAI_AZURE_DALLE_API_VERSION}`;
 };
 
+/**
+ * textCompletionUrl
+ * @description This function returns the URL for text completion using Azure OpenAI's API.
+ * @example const url = textCompletionUrl();
+ * @returns {string} The URL for text completion.
+ */
 export const textCompletionUrl = (): string => {
   // check that all required environment variables are set
   if (
@@ -131,6 +151,19 @@ export const textCompletionUrl = (): string => {
   return `${OPENAI_BASE_PATH}/openai/deployments/${OPENAI_AZURE_MODEL_DEPLOYMENT}/completions?api-version=${OPENAI_AZURE_API_VERSION}`;
 };
 
+/**
+ * submitImageGenerationRequest
+ * @description This function submits a request to generate an image using OpenAI's DALL-E API.
+ * @example const image = await submitImageGenerationRequest({ prompt: 'a prompt' });
+ * @param {SubmitImageGenerationRequestParams} params - object with parameters for the request.
+ * @param {string} params.prompt - The prompt for the image generation.
+ * @param {string} [params.apiUrl] - The URL for the API endpoint.
+ * @param {string} [params.resolution] - The resolution of the generated image.
+ * @param {string} [params.format] - The format of the generated image.
+ * @param {number} [params.numImages] - The number of images to generate.
+ * @param {Headers} [params.requestHeaders] - The headers for the request.
+ * @returns {Promise<string>} A promise that resolves with the generated image.
+ */
 export const submitImageGenerationRequest = async ({
   prompt: caption,
   apiUrl: url = imageGenerationUrl(),
@@ -162,6 +195,15 @@ export const submitImageGenerationRequest = async ({
   return imageOperationUrl;
 };
 
+/**
+ * getImageGenerationResult
+ * @description This function gets the result of an image generation request using OpenAI's DALL-E API.
+ * @example const image = await getImageGenerationResult({ operationUrl: 'an operation url' });
+ * @param {GetImageGenerationResultParams} params - object with parameters for the request.
+ * @param {string} params.operationUrl - The URL for the operation.
+ * @param {Headers} [params.requestHeaders] - The headers for the request.
+ * @returns {Promise<string>} A promise that resolves with the generated image.
+ */
 export const getImageGenerationResult = async ({
   imageOperationUrl: url,
   requestHeaders = defaultHeaders(),
@@ -196,6 +238,16 @@ export const getImageGenerationResult = async ({
   return imageUrl;
 };
 
+/**
+ * saveImageGenerationResult
+ * @description This function saves the generated image to a file.
+ * @param {SaveImageGenerationResultParams} params - The parameters for saving the image.
+ * @param {string} params.imageUrl - The URL of the generated image.
+ * @param {string} [params.imageName=uuid()] - The name of the image file to save.
+ * @param {string} [params.saveToPath='./src/dall-e-image-rest/generated-images/'] - The path to save the image file to.
+ * @param {Record<string, string>} [params.requestHeaders=defaultHeaders()] - The headers for the image request.
+ * @returns {Promise<string>} A promise that resolves with the full path to the saved image file.
+ */
 export const saveImageGenerationResult = async ({
   imageUrl: url,
   imageName = uuid(),
@@ -224,6 +276,17 @@ export const saveImageGenerationResult = async ({
   return fullImagePath;
 };
 
+/**
+ * doTextCompletion
+ * @description This function provides text completion suggestions for the given prompt using OpenAI's API.
+ * @param {TextCompletionParams} params - The parameters for the text completion request.
+ * @param {string} params.prompt - The prompt for the text completion request.
+ * @param {string} [params.apiUrl=textCompletionUrl()] - The URL for the text completion API.
+ * @param {Record<string, string>} [params.requestHeaders=defaultHeaders()] - The headers for the text completion request.
+ * @param {number} [params.maxTokens=1000] - The maximum number of tokens to generate for the text completion.
+ * @param {number} [params.temperature=0.2] - The sampling temperature to use for the text completion.
+ * @returns {Promise<string>} A promise that resolves with the generated text completion.
+ */
 export const doTextCompletion = async ({
   prompt,
   apiUrl: url = textCompletionUrl(),
@@ -260,6 +323,13 @@ export const doTextCompletion = async ({
   return completion;
 };
 
+/**
+ * generateAndSaveImage
+ * @description This function submits a request to generate an image using OpenAI's DALL-E API and saves the result to a file.
+ * @example const image = await generateAndSaveImage('a prompt');
+ * @param {string} prompt - The prompt for the image generation.
+ * @returns {Promise<string>} A promise that resolves with the path to the saved image.
+ */
 export const generateAndSaveImage = async (prompt: string): Promise<string> => {
   // eslint-disable-next-line @typescript-eslint/await-thenable
   const imageOperationUrl = await submitImageGenerationRequest({
