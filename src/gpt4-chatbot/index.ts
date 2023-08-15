fa2a5f14db780914611114d96b1509404b024c5b
Michael Sprague <mikesprague@gmail.com>
2023-08-14 22:22:29 -0400

    feat: add gpt4-chatbot demo

---
+++ src/gpt4-chatbot/index.ts
@@ -0,0 +1,90 @@
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+import { oneLineTrim } from 'common-tags';
+import readline from 'node:readline';
+import OpenAI from 'openai';
+
+import { showGoodbye } from '../lib/helpers.js';
+
+// load environment variables from .env file
+dotenv.config();
+
+// destructure environment variables we need
+const {
+  // OPENAI_AZURE_API_VERSION,
+  OPENAI_GPT4_API_KEY,
+  OPENAI_GPT4_BASE_PATH,
+  OPENAI_GPT4_AZURE_MODEL_DEPLOYMENT,
+} = process.env;
+
+// check that all required environment variables are set
+if (
+  !OPENAI_GPT4_API_KEY ||
+  !OPENAI_GPT4_BASE_PATH ||
+  !OPENAI_GPT4_AZURE_MODEL_DEPLOYMENT
+) {
+  throw new Error(
+    oneLineTrim`
+      Missing one or more required environment variables:
+
+      OPENAI_API_KEY, OPENAI_AZURE_DALLE_API_VERSION, OPENAI_AZURE_MODEL_DEPLOYMENT
+    `,
+  );
+  process.exit(1);
+}
+
+readline.emitKeypressEvents(process.stdin);
+
+process.stdin.on('keypress', (ch, key: KeyPressKey) => {
+  if (key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+    console.log(showGoodbye());
+    process.exit(0);
+  }
+});
+
+// instantiate the OpenAI client for Azure OpenAI
+const openAI = new OpenAI({
+  apiKey: OPENAI_GPT4_API_KEY,
+  baseURL: `${OPENAI_GPT4_BASE_PATH}openai/deployments/${OPENAI_GPT4_AZURE_MODEL_DEPLOYMENT}`,
+  defaultQuery: { 'api-version': '2023-06-01-preview' },
+  defaultHeaders: { 'api-key': OPENAI_GPT4_API_KEY },
+});
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.clear();
+console.log(
+  '🤖',
+  chalk.bold.italic.green('Connection established... begin chatting!'),
+);
+userInterface.prompt();
+
+const messages = [
+  { role: 'system', content: 'You are a helpful AI assistant.' },
+];
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+userInterface.on('line', async (input) => {
+  messages.push({ role: 'user', content: input });
+  let stream;
+  try {
+    stream = await openAI.chat.completions.create({
+      model: OPENAI_GPT4_AZURE_MODEL_DEPLOYMENT,
+      messages,
+      stream: true,
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+  // console.log(stream);
+  for await (const part of stream) {
+    process.stdout.write(chalk.blue(part.choices[0]?.delta?.content || ''));
+  }
+  process.stdout.write('\n');
+});
